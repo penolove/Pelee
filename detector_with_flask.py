58e4a81527609da5c619b8f2822f8e7df9422bb0
penolove <chinghua.yang@appier.com>
2018-12-29 21:19:51 +0800

    add flake8 setting, end2end detector, detector with flask

---
+++ detector_with_flask.py
@@ -0,0 +1,98 @@
+import argparse
+import os
+
+import caffe
+from eyewitness.config import BBOX
+from eyewitness.flask_server import BboxObjectDetectionFlaskWrapper
+from eyewitness.result_handler.db_writer import BboxPeeweeDbWriter
+from eyewitness.result_handler.line_detection_result_handler import LineAnnotationSender
+from naive_detector import PeLeeDetectorWrapper
+from peewee import SqliteDatabase
+
+
+# class YOLO defines the default value, so suppress any default here
+parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+'''
+Command line options
+'''
+parser.add_argument('--gpu_id', type=int, default=0)
+parser.add_argument('--repo_path', type=str, default='/opt/caffe/examples/pelee')
+parser.add_argument(
+    '--db_path', type=str, default='::memory::',
+    help='the path used to store detection result records'
+)
+parser.add_argument(
+    '--interval_s', type=int, default=3, help='the interval of image generation'
+)
+parser.add_argument(
+    '--detector_host', type=str, default='localhost', help='the ip address of detector'
+)
+parser.add_argument(
+    '--detector_port', type=int, default=5566, help='the port of detector port'
+)
+parser.add_argument(
+    '--drawn_image_dir', type=str, default=None,
+    help='the path used to store drawn images'
+)
+
+
+def image_url_handler(drawn_image_path):
+    """if site_domain not set in env, will pass a pickchu image"""
+    site_domain = os.environ.get('site_domain')
+    if site_domain is None:
+        return 'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png'
+    else:
+        return '%s/%s' % (site_domain, drawn_image_path)
+
+
+def line_detection_result_filter(detection_result, threshold=0.6):
+    """
+    used to check if sent notification or not
+    """
+    return any(i.label == 'person' and i.score > threshold
+               for i in detection_result.detected_objects)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    # initialize object_detector
+    if args.gpu_id >= 0:
+        caffe.set_device(args.gpu_id)
+        caffe.set_mode_gpu()
+
+    # setup your detector
+    repo_path = args.repo_path
+    params = {
+        'labelmap_file': os.path.join(repo_path, 'model/voc/labelmap_voc.prototxt'),
+        'model_def': os.path.join(repo_path, 'model/voc/deploy_merged.prototxt'),
+        'model_weights': os.path.join(repo_path, 'model/voc/pelee_merged.caffemodel'),
+    }
+    object_detector = PeLeeDetectorWrapper(params, threshold=0.6)
+
+    # detection result handlers
+    result_handlers = []
+
+    # update image_info drawn_image_path, insert detection result
+    database = SqliteDatabase(args.db_path)
+    bbox_sqlite_handler = BboxPeeweeDbWriter(database)
+    result_handlers.append(bbox_sqlite_handler)
+
+    # setup your line channel token and audience
+    channel_access_token = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+    if channel_access_token:
+        line_annotation_sender = LineAnnotationSender(
+            channel_access_token=channel_access_token,
+            image_url_handler=image_url_handler,
+            detection_result_filter=line_detection_result_filter,
+            detection_method=BBOX,
+            update_audience_period=10,
+            database=database)
+        result_handlers.append(line_annotation_sender)
+
+    flask_wrapper = BboxObjectDetectionFlaskWrapper(
+        object_detector, bbox_sqlite_handler, result_handlers,
+        database=database, drawn_image_dir=args.drawn_image_dir)
+
+    params = {'host': args.detector_host, 'port': args.detector_port, }
+    flask_wrapper.app.run(**params)
---
+++ end2end_detector.py
@@ -0,0 +1,124 @@
+import argparse
+import os
+
+import arrow
+import cv2
+import time
+import caffe
+from eyewitness.config import (IN_MEMORY, BBOX)
+from eyewitness.image_id import ImageId
+from eyewitness.image_utils import (ImageProducer, swap_channel_rgb_bgr, ImageHandler)
+from eyewitness.result_handler.db_writer import BboxPeeweeDbWriter
+from eyewitness.result_handler.line_detection_result_handler import LineAnnotationSender
+from naive_detector import PeLeeDetectorWrapper
+from peewee import SqliteDatabase
+from PIL import Image
+
+
+# class YOLO defines the default value, so suppress any default here
+parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+'''
+Command line options
+'''
+parser.add_argument('--gpu_id', type=int, default=0)
+parser.add_argument('--repo_path', type=str, default='/opt/caffe/examples/pelee')
+parser.add_argument(
+    '--db_path', type=str, default='::memory::',
+    help='the path used to store detection result records'
+)
+parser.add_argument(
+    '--interval_s', type=int, default=3, help='the interval of image generation'
+)
+
+
+class InMemoryImageProducer(ImageProducer):
+    def __init__(self, video_path, interval_s):
+        self.vid = cv2.VideoCapture(video_path)
+        self.interval_s = interval_s
+        if not self.vid.isOpened():
+            raise IOError("Couldn't open webcam or video")
+
+    def produce_method(self):
+        return IN_MEMORY
+
+    def produce_image(self):
+        while True:
+            # clean buffer hack: for Linux V4L capture backend with a internal fifo
+            for iter_ in range(5):
+                self.vid.grab()
+            _, frame = self.vid.read()
+            yield Image.fromarray(swap_channel_rgb_bgr(frame))
+            time.sleep(self.interval_s)
+
+
+def image_url_handler(drawn_image_path):
+    """if site_domain not set in env, will pass a pickchu image"""
+    site_domain = os.environ.get('site_domain')
+    if site_domain is None:
+        return 'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png'
+    else:
+        return '%s/%s' % (site_domain, drawn_image_path)
+
+
+def line_detection_result_filter(detection_result):
+    """
+    used to check if sent notification or not
+    """
+    return any(i.label == 'person' for i in detection_result.detected_objects)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # image producer from webcam
+    image_producer = InMemoryImageProducer(0, interval_s=args.interval_s)
+
+    # gpu preparation
+    if args.gpu_id >= 0:
+        caffe.set_device(args.gpu_id)
+        caffe.set_mode_gpu()
+    
+    # setup your detector
+    repo_path = args.repo_path
+    params = {
+        'labelmap_file': os.path.join(repo_path, 'model/voc/labelmap_voc.prototxt'),
+        'model_def': os.path.join(repo_path, 'model/voc/deploy_merged.prototxt'),
+        'model_weights': os.path.join(repo_path, 'model/voc/pelee_merged.caffemodel'),
+    }
+    object_detector = PeLeeDetectorWrapper(params, threshold=0.6)
+
+    # detection result handlers
+    result_handlers = []
+
+    # update image_info drawn_image_path, insert detection result
+    database = SqliteDatabase(args.db_path)
+    bbox_sqlite_handler = BboxPeeweeDbWriter(database)
+    result_handlers.append(bbox_sqlite_handler)
+
+    # setup your line channel token and audience
+    channel_access_token = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+    if channel_access_token:
+        line_annotation_sender = LineAnnotationSender(
+            channel_access_token=channel_access_token,
+            image_url_handler=image_url_handler,
+            detection_result_filter=line_detection_result_filter,
+            detection_method=BBOX,
+            update_audience_period=10,
+            database=database)
+        result_handlers.append(line_annotation_sender)
+
+    for image in image_producer.produce_image():
+        image_id = ImageId(channel='demo', timestamp=arrow.now().timestamp, file_format='jpg')
+        bbox_sqlite_handler.register_image(image_id, {})
+        detection_result = object_detector.detect(image, image_id)
+
+        # draw and save image, as object detected update detection result
+        if len(detection_result.detected_objects) > 0:
+            drawn_image_path = "detected_image/%s_%s.%s" % (
+                image_id.channel, image_id.timestamp, image_id.file_format)
+            ImageHandler.draw_bbox(image, detection_result.detected_objects)
+            ImageHandler.save(image, drawn_image_path)
+            detection_result.image_dict['drawn_image_path'] = drawn_image_path
+
+        for result_handler in result_handlers:
+            result_handler.handle(detection_result)
